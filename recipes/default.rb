34ab06bf07b8f7eb8f02c610e055c419c0a90bf7
asen15 <aniruddha.sen15@gmail.com>
2018-10-19 13:25:05 -0400

    chef-client enabled to process every 5 minutes

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+#
+#
+
+node.default['chef_client']['interval'] = '300'
+node.default['chef_client']['splay'] = '60'
+
+include_recipe 'chef-client::default'
